23fafdc0214507c6197220c172df17b04fcba4b9
Arny <tucvbn@gmail.com>
2024-04-10 18:32:37 +0700

    update: cau 3.4, word

--- Cau3.4.py
+++ Cau3.4.py
@@ -8,8 +8,8 @@ spark = SparkSession.builder \
 vdv_olympics_df = spark.read.csv(r'D:\Y3\BigData\BigData\DeTaiSo14\Data\vdv_olympics.csv', header=True, inferSchema=True)
 qg_noc_df = spark.read.csv(r'D:\Y3\BigData\BigData\DeTaiSo14\Data\qg_noc.csv', header=True, inferSchema=True)
 
-athlete_counts_by_year = vdv_olympics_df.groupBy("Year").agg(countDistinct("ID").alias("AthleteCount"))
-recent_olympics = athlete_counts_by_year.orderBy("Year", ascending=False).limit(5)
+athlete_counts_by_year = vdv_olympics_df.groupBy("Games").agg(countDistinct("ID").alias("AthleteCount"))
+recent_olympics = athlete_counts_by_year.orderBy("Games", ascending=False).limit(5)
 recent_olympics.show()
 
 spark.stop()
